5d8f0208e06010113ff6f27f338f5ee6fb8f9b1f
Simon Kim <simonkim008@gmail.com>
2023-07-14 17:07:09 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage':{
+        'birthName': 'Sheyaa',
+        'birthLocation': 'London',
+        'age': 29
+    },
+
+    'chance the rapper':{
+        'birthName': 'Chancelor',
+        'birthLocation': 'Chicago',
+        'age': 29
+    },
+
+    'unknown':{
+        'birthName': 'unknown',
+        'birthLocation': 'unknown',
+        'age': 0
+    }
+    
+}
+
+app.get('/', (request, response) =>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request, response)=>{
+    const rapperName = request.params.name.toLowerCase()
+    if(rappers[rapperName]){
+        response.json(rappers[rapperName])
+    }else{
+        response.json(rappers['unknown'])
+    }
+    
+})
+
+app.listen(PORT, ()=>{
+    console.log(`the server is now running on port ${PORT}! Betta Go catch it`)
+})
